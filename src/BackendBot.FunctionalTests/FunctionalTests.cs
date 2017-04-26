c1614d7b9d978c1f4cc112394df09cac8b5c0b67
Unknown <clencu_007@yahoo.com>
2017-04-26 14:28:52 +0300

    added unit tests projects

---
+++ src/BackendBot.FunctionalTests/FunctionalTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BackendBot.FunctionalTests
+{
+    [TestClass]
+    public class FunctionalTests
+    {
+        [TestMethod]
+        public void OneEqualsOne_FromFunctionalTests()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
